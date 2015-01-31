53ef903164eb9091fe71fcf61f4a9d06adc55384
ATIME <1018764394@qq.com>
2015-01-31 12:34:15 +0800

    GPIO stuudy

---
+++ no-OS/GPIO/S3C2440.h
@@ -0,0 +1,14 @@
+#ifndef __S3C2440_H
+#define __S3C2440_H
+
+#define GPBCON (*(volatile unsigned *)0x56000010)
+#define GPBDAT (*(volatile unsigned *)0x56000014)
+#define GPBUP  (*(volatile unsigned *)0x56000018)
+
+#define GPFCON (*(volatile unsigned *)0x56000050)
+#define GPFDAT (*(volatile unsigned *)0x56000054)
+#define GPFUP  (*(volatile unsigned *)0x56000058)
+
+
+#endif 
+
---
+++ no-OS/GPIO/leds.c
@@ -0,0 +1,43 @@
+#include "S3C2440.h"	//非官方头文件，仅对当前程序有效
+
+#define	GPB5_out	(1<<(5*2))
+#define	GPB6_out	(1<<(6*2))
+#define	GPB7_out	(1<<(7*2))
+#define	GPB8_out	(1<<(8*2))
+						//设置GPIO
+
+int atime(void)			//程序入口
+{
+	unsigned long i = 0;
+	GPFCON = 0X0;
+	GPFUP  = 0X0;
+
+	GPBCON = GPB5_out | GPB6_out | GPB7_out | GPB8_out;
+
+	while(1){
+		i =GPFDAT;
+		
+		if( i&(1<<0) )
+			GPBDAT |=(1<<5);
+		else
+			GPBDAT &=~(1<<5);
+
+		if( i&(1<<1) )
+			GPBDAT |=(1<<6);
+		else
+			GPBDAT &=~(1<<6);
+
+		if( i&(1<<2) )
+			GPBDAT |=(1<<7);
+		else
+			GPBDAT &=~(1<<7);
+
+		if( i&(1<<4) )
+			GPBDAT |=(1<<8);
+		else
+			GPBDAT &=~(1<<8);
+
+	}
+
+	return 0;
+}
